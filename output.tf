c661cd57ab9894c8b80dfda6c20f880406420e2a
Nishant <nishant.lakhanitechnologies@gmail.com>
2025-07-15 17:01:13 +0530

    added outputs

---
+++ output.tf
@@ -0,0 +1,15 @@
+output "template_id" {
+    value = aws_launch_template.this.id
+}
+
+output "template_id" {
+    value = aws_launch_template.this.name
+}
+
+output "asg_id" {
+    value = aws_autoscaling_group.this.id
+}
+
+output "asg_name" {
+    value = aws_autoscaling_group.this.name
+}
